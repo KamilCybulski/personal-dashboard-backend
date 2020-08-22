2f72e9938c031c5cb774f3953d82eedf48ce19db
Kamil <kamillcybulski@gmail.com>
2020-08-22 14:02:14 +0200

    User data endpoint (#2)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { UserController } from './user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [UserService],
   exports: [UserService],
+  controllers: [UserController],
 })
-export class UserModule {}
+export class UserModule { }
