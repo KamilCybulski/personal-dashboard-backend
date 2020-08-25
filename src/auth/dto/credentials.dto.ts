ff8766550785b50aff0b6f3062f3dfafafc77be9
Kamil <kamillcybulski@gmail.com>
2020-08-25 18:18:42 +0200

    Add docs (#3)

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -1,9 +1,18 @@
 import { Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CredentialsDTO {
+  @ApiProperty({
+    minLength: 3,
+    maxLength: 20,
+  })
   @Length(3, 20)
   name: string;
 
+  @ApiProperty({
+    minLength: 6,
+    maxLength: 20,
+  })
   @Length(6, 20)
   password: string;
 }
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class UserDTO {
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   name: string;
 }
